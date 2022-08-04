c8be4a8828a1d6cb941422bec81d1c8e665183fa
Muhammad Arif Fathurohman <mariffathurohman@gmail.com>
2022-08-04 20:07:25 +0700

    style: setting up typography

---
+++ src/themes/typography.js
@@ -0,0 +1,7 @@
+const fontFamily = {
+  nunitoLight: 'NunitoSans-Light',
+  nunitoRegular: 'NunitoSans-Regular',
+  nunitoSemi: 'NunitoSans-SemiBold',
+  nunitoBold: 'NunitoSans-Bold',
+};
+export default fontFamily;
